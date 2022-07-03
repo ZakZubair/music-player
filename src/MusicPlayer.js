a7444a209ecfc3bd30f57bce0189c837db2bd674
ΖΛΚ <zakaaf@hotmail.co.uk>
2022-07-03 14:23:06 +0100

    Initial Commit

---
+++ src/MusicPlayer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './main.scss'
+
+const MusicPlayer = () => {
+    return (
+        <div className="main">
+            <h1>Music Player 🎵</h1>
+        </div>
+    )
+}
+
+export default MusicPlayer
